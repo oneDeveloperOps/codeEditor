17b6475bbe9bae247edaad57c41124ed30f4e2ef
oneDeveloperOps <devopseduo@gmail.com>
2020-03-17 13:08:38 +0530

    Connected MongoDb

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
+const mongoDb = require('./config/db')
 
 app.use('/' , (req , res) => {
     res.send('<h1>Hello</h1>')
 })
 
+mongoDb();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => { console.log(`server running at port ${PORT}`) });
---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect(db , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('connected to db')
+    }catch(err) {
+        console.log(err.code);
+        process.exit(1);
+    }
+}
+
+module.exports = mongoConnect;
